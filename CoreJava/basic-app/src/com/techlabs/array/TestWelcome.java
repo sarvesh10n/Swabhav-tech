b904c0f3b431b24cf93e78d1fb8ca4ed776ee7f6
sarvesh nerurkar <sarvesh10n@outlook.com>
2017-12-26 18:47:31 +0530

    input taken through args

--- CoreJava/basic-app/src/com/techlabs/array/TestWelcome.java
+++ CoreJava/basic-app/src/com/techlabs/array/TestWelcome.java
@@ -2,10 +2,12 @@ package com.techlabs.array;
 
 public class TestWelcome {
 
-	public static void main(String args[])
-	{
-		
-			System.out.println("Hello world");
-	
-}
+	public static void main(String names[]) {
+
+		if (names.length == 0)
+			System.out.println("Pass some names");
+		else
+			for(String i:names)
+				System.out.println(i);
+	}
 }
