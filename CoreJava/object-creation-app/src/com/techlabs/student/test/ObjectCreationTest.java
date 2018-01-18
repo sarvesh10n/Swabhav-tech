fcafd5a6833870a7a09212bb5aa4946892de49b7
sarvesh nerurkar <sarvesh10n@outlook.com>
2018-01-18 22:43:30 +0530

    added different ways to create object code

---
+++ CoreJava/object-creation-app/src/com/techlabs/student/test/ObjectCreationTest.java
@@ -0,0 +1,66 @@
+package com.techlabs.student.test;
+
+import com.techlabs.student.*;
+
+import java.lang.reflect.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ObjectCreationTest {
+
+	public static void main(String args[]) {
+
+		objectCreation1();
+
+		objectCreation2();
+
+		objectCreation3();
+	}
+
+	private static void objectCreation1() {
+		Student student;
+		try {
+			student = (Student) Class.forName(
+					"com.techlabs.student.Student").newInstance();
+			student.setAddress("Bhandup");
+			student.setFirstName("Sarvesh");
+			student.setLastName("Nerurkar");
+			student.displayStudentDetails();
+		} catch (InstantiationException | IllegalAccessException
+				| ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	private static void objectCreation2() {
+		Student student = new Student();
+		try {
+			Student studentClone = (Student) student.clone();
+			studentClone.setAddress("Bhandup");
+			studentClone.setFirstName("Sarvesh");
+			studentClone.setLastName("Nerurkar");
+			studentClone.displayStudentDetails();
+		} catch (CloneNotSupportedException e) {
+
+		}
+
+	}
+
+	private static void objectCreation3() {
+		try {
+			Constructor<Student> constructor = Student.class
+					.getDeclaredConstructor();
+			
+			Student student=constructor.newInstance();
+			
+			student.setAddress("Bhandup");
+			student.setFirstName("Sarvesh");
+			student.setLastName("Nerurkar");
+			student.displayStudentDetails();
+
+		} catch (Exception e) {
+
+		}
+	}
+
+}
