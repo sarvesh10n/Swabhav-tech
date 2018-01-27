89dd74240c3dad1a32d0820bc1147a0223d66610
sarvesh nerurkar <sarvesh10n@outlook.com>
2018-01-27 12:05:07 +0530

    Added ooad guitar app

---
+++ OOAD/guitar-app/src/com/techlabs/guitar/search/Type.java
@@ -0,0 +1,17 @@
+package com.techlabs.guitar.search;
+
+public enum Type {
+
+	ACOUSTIC, ELECTRIC;
+
+	public String toString() {
+		switch (this) {
+		case ACOUSTIC:
+			return "acoustic";
+		case ELECTRIC:
+			return "electric";
+		default:
+			return "unspecified";
+		}
+	}
+}
